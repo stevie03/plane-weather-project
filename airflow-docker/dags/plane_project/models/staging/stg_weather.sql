09430f601ec9f1d5dc33a1f85806513f4fbd8d4f
Stevie <162144874+stevie03@users.noreply.github.com>
2026-06-24 12:09:57 +0200

    Remove comment from stg_weather.sql
    Remove commented line explaining timestamp parsing.

--- airflow-docker/dags/plane_project/models/staging/stg_weather.sql
+++ airflow-docker/dags/plane_project/models/staging/stg_weather.sql
@@ -8,7 +8,6 @@ SELECT
     city,
     latitude,
     longitude,
-    -- Ez a varázslat: A szöveges '20260423_144512' formátumból csinál egy szabványos SQL dátumot!
     PARSE_TIMESTAMP('%Y%m%d_%H%M%S', extraction_timestamp) AS measured_at,
     temperature_celsius,
     wind_speed_kmh,
